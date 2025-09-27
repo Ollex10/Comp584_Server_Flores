0e63363c7cb41af81098ad4158b13e14f0d8fb5c
ollex10 <flores.isai66@gmail.com>
2025-09-26 18:18:08 -0700

    Added WorldModel and Database Stuff

---
+++ WorldModel/City.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace WorldModel;
+
+[Table("City")]
+public partial class City
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Column("CountryID")]
+    public int CountryId { get; set; }
+
+    [StringLength(50)]
+    [Unicode(false)]
+    public string CityName { get; set; } = null!;
+
+    public int Latitude { get; set; }
+
+    public int Longitude { get; set; }
+
+    public int Population { get; set; }
+
+    [ForeignKey("CountryId")]
+    [InverseProperty("Cities")]
+    public virtual Country Country { get; set; } = null!;
+}
