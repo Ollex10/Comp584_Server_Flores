52669f89e16ba0581b59007aa35866737a4b1969
ollex10 <flores.isai66@gmail.com>
2025-10-03 17:50:13 -0700

    Fixed Swagger issue also added More controllers and other things like that

---
+++ Controllers/SeedController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WorldModel;
+
+namespace Comp584_Server_Flores.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SeedController(Comp584Context context) : ControllerBase
+    {
+        [HttpPost ("Countries")]
+        public async Task<ActionResult> PostCountries()
+        {
+            
+            await context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        [HttpPost ("Cities")]
+        public async Task<ActionResult> PostCities()
+        {
+
+            await context.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,19 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using WorldModel;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
+builder.Services.AddDbContext<Comp584Context>(options => {
+
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "My API", Version = "v1" });
-    // Optional: Include XML comments for better documentation
-    // var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    // var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-    // c.IncludeXmlComments(xmlPath);
-});
+builder.Services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo { Title = "hello", Version = "v1" }));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,10 +22,7 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
     app.UseSwagger();
-    app.UseSwaggerUI(options =>
-    {
-        options.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-    });
+    app.UseSwaggerUI();
 }
 
 app.UseCors(options => options.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
